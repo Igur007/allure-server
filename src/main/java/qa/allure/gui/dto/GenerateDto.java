520d66a35f46f18ec1ebe834fb500646eb1275e7
Ivan H <igur007@gmail.com>
2024-11-08 17:04:30 +0200

    fix readme

---
+++ src/main/java/qa/allure/gui/dto/GenerateDto.java
@@ -0,0 +1,24 @@
+package qa.allure.gui.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import qa.allure.service.PathUtil;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenerateDto {
+
+    @NotEmpty
+    @Pattern(regexp = PathUtil.UUID_PATTERN)
+    String resultUuid;
+    @Size(min = 2, max = 120)
+    String path;
+    @Size(min = 2, max = 120)
+    String build;
+    boolean deleteResults;
+}
